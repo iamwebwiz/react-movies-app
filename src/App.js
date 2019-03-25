4f564254eb458386f604a9c62fedb393efe30b43
Ezekiel <iamwebwiz@gmail.com>
2019-03-25 06:30:18 +0100

    update App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
