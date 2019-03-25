8eb1359e4396f59f330ec3b5cd2065db278a160a
Ezekiel <iamwebwiz@gmail.com>
2019-03-25 06:24:51 +0100

    add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Hello</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
