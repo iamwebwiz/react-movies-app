79d33ae8eca46f2496b2d49f096f3b565e2c1c3c
Ezekiel <iamwebwiz@gmail.com>
2019-03-31 03:49:28 +0100

    Update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import { Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Home extends Component {
   state = {
@@ -14,6 +8,10 @@ class Home extends Component {
   };
 
   componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
     fetch(this.state.baseURL)
       .then((res) => res.json())
       .then((movies) => {
@@ -25,11 +23,29 @@ class Home extends Component {
   }
 
   render() {
+    const { movies } = this.state;
+
     return (
       <div>
         <h1>React Movies App</h1>
         <hr />
         <h3>Available Movies</h3>
+
+        <div className="row">
+          {movies.map((movie) => {
+            return (
+              <Card>
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>{movie.title}</CardTitle>
+                  <CardSubtitle>Genre: {movie.genre}</CardSubtitle>
+                  <CardText>{movie.synopsis}</CardText>
+                  <Button>Watch Movie</Button>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     );
   }
