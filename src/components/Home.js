760aba742b65ae678167bab55a51a6e1a4e883e3
Ezekiel <iamwebwiz@gmail.com>
2019-03-31 03:18:27 +0100

    Update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  state = {
+    movies: [],
+    baseURL: 'https://simplecrudapi.herokuapp.com/api/movies'
+  };
+
+  componentDidMount() {
+    fetch(this.state.baseURL)
+      .then((res) => res.json())
+      .then((movies) => {
+        this.setState({
+          movies: movies
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
     return (
-      <div className="container">
-        <h3>Hello</h3>
+      <div>
+        <h1>React Movies App</h1>
+        <hr />
+        <h3>Available Movies</h3>
       </div>
     );
   }
