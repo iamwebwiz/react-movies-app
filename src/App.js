c8712301aa677cc495b23029e42a3de26fe3529f
Ezekiel <iamwebwiz@gmail.com>
2019-03-31 03:18:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="mt-5">
+          <div className="row">
+            <div className="col-12">
+              <Home />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
