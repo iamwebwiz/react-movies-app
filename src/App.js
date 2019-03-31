05f10849a1c2d5ccfa9af4f449b230ad353b4320
Ezekiel <iamwebwiz@gmail.com>
2019-03-31 05:28:04 +0100

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react"
+import Home from "./components/Home"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class App extends Component {
   render() {
@@ -14,8 +14,8 @@ class App extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
