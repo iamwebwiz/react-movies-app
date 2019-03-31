030ed53ad37fa9e42a7fbe8b1e07ef865ba343fb
Ezekiel <iamwebwiz@gmail.com>
2019-03-31 05:28:24 +0100

    Update Home component and Index.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,80 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import React, { Component } from "react"
+import NewMovieModal from "./partial/NewMovieModal"
+import { Button, Card, CardGroup } from "react-bootstrap"
 
 class Home extends Component {
-  state = {
-    movies: [],
-    baseURL: 'https://simplecrudapi.herokuapp.com/api/movies'
-  };
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      baseURL: "https://simplecrudapi.herokuapp.com/api/movies",
+      show: false
+    }
+    this.handleShow = this.handleShow.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
 
   componentDidMount() {
-    this.fetchMovies();
+    this.fetchMovies()
+  }
+
+  handleClose() {
+    this.setState({ show: false })
+  }
+
+  handleShow() {
+    this.setState({ show: true })
   }
 
   fetchMovies() {
     fetch(this.state.baseURL)
-      .then((res) => res.json())
-      .then((movies) => {
+      .then(res => res.json())
+      .then(movies => {
         this.setState({
           movies: movies
-        });
+        })
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error))
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies } = this.state
+    const moviesList = movies.map((movie, index) => {
+      return (
+        <Card key={index}>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.synopsis}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Genre: {movie.genre}</small>
+          </Card.Footer>
+        </Card>
+      )
+    })
 
     return (
       <div>
         <h1>React Movies App</h1>
         <hr />
-        <h3>Available Movies</h3>
-
         <div className="row">
-          {movies.map((movie) => {
-            return (
-              <Card>
-                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                <CardBody>
-                  <CardTitle>{movie.title}</CardTitle>
-                  <CardSubtitle>Genre: {movie.genre}</CardSubtitle>
-                  <CardText>{movie.synopsis}</CardText>
-                  <Button>Watch Movie</Button>
-                </CardBody>
-              </Card>
-            );
-          })}
+          <div className="col-auto">
+            <h3>Available Movies</h3>
+          </div>
+          <div className="col-auto">
+            <Button variant="primary" onClick={this.handleShow}>
+              Upload Movie
+            </Button>
+          </div>
         </div>
+        <div className="row">
+          <CardGroup>{moviesList}</CardGroup>
+        </div>
+        <NewMovieModal show={this.state.show} onHide={this.handleClose} close={this.handleClose} />
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
